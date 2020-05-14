b1bdda095a2cdfdd8caf22a06e213be4c863c267
Aleks Popov <popovaleks444@gmail.com>
2020-05-14 14:53:47 +0300

    Added scene transitions and core game UI elements.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,9 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    private int STARTING_SCENE_INDEX = 0;
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartingScene()
+    {
+        SceneManager.LoadScene(STARTING_SCENE_INDEX);
     }
 }

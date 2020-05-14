b6453d84aa2a17143c8dfb4478729cefaffcf64c
Aleks Popov <popovaleks444@gmail.com>
2020-05-14 16:23:31 +0300

    Added number wizzard gameplay script. Added functionality to higher and lower buttons. Guess updates after every click.

---
+++ Assets/Scripts/NumberWizardConsole.cs
@@ -0,0 +1,42 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class NumberWizardConsole : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] private TextMeshProUGUI guessText;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        guess = (max + min) / 2;
+
+        guessText.text = guess.ToString();
+        max = max + 1;
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+}
